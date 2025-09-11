33aa36d9b762055a78f1056faf25b9b5bce8186c
SudhinRj <rjsudhin@users.noreply.github.com>
2025-09-11 15:03:38 +0530

    Game is working fine with unstyled ui

--- js/script.js
+++ js/script.js
@@ -1,145 +1,107 @@
-const userScoreDisplay = document.querySelector('.userScore')
-const computerScoreDisplay = document.querySelector('.computerScore')
-const gameStartBtn = document.querySelector('.GameStartButton')
-const winnerAlert = document.querySelector('.winner-alert')
+const userScoreDisplay = document.querySelector('.user-score-display')
+const computerScoreDisplay = document.querySelector('.computer-score-display')
+const gameCards = document.querySelector('.game-cards')
 
-let humanScore = 0
+const gameController = document.querySelector('.game-controller')
+
+const userCard = document.querySelector('.user-card')
+const computerCard = document.querySelector('.computer-card')
+const gameWinnerDisplay = document.querySelector('.game-winner-display')
+let userScore = 0
 let computerScore = 0
+let countingGame = 0
 
-// getting the computer choice
-function getComputerChoice() {
-  let randomNumber = Math.floor(Math.random() * 3) + 1
-  // returning random valids choices
-  let choice = ''
-  switch (randomNumber) {
-    case 1: 
-      choice = 'rock'
-      break
-    case 2: 
-      choice = 'paper'
-      break
-    case 3: 
-      choice = 'scissors'
-      break
-  }
+// game begins
+gameController.addEventListener('click', (e) => {
+  playGame(e)
+})
 
-  return choice
-}
-
-
-// getting the human | user choice 
-function getHumanChoice() {
-  let inputResult = false
-  while (!inputResult) {
-    let userInput = prompt("You should 'rock', 'paper', 'scissors'")
-    userInput = userInput.toLowerCase()
-    if (
-      (userInput == 'rock') ||
-      (userInput == 'paper') ||
-      (userInput == 'scissors')
-    ) {
-      console.log('the valid choices done')
-      inputResult = true
-      return userInput
-    } else {
-      console.log('please enter the valid choices')
-    }
-  }
-}
-
-// making values to capitalize
-function getCapitalizeResult(str) {
-  let result = str.charAt(0).toUpperCase()
-  result += str.slice(1).toLowerCase()
-  return result
-}
-
-// playing round checking choices adding scores
-function playRound(humanChoice, computerChoice) {
+function playGame(e) {
+  countingGame++
+  console.log(countingGame)
+  console.log(e.target.id)
+  let userSelection = gettingUserSelection(e.target.id)
+  let computerSelection = gettingComputerSelection()
   
-  console.log(`Comptuer selected "${computerChoice}" human selected "${humanChoice}"`)
-  // checking the game rounds choices
-  if (humanChoice === computerChoice) {
-    console.log('Tie')
-    
+  playRound(userSelection, computerSelection)
+  if (countingGame == 5) {
+    anouncingGameWinner()
+  }
+}
+
+// reset game things and scores
+function resetGame(e) {
+  userScore = 0
+  computerScore = 0
+  countingGame = 0
+  userScoreDisplay.textContent = ''
+  computerScoreDisplay.textContent = ''
+  gameWinnerDisplay.textContent = ''
+  console.log(computerScoreDisplay)
+  gameCards.removeChild(e.target)
+
+}
+
+// anouncing the winner 
+function anouncingGameWinner() {
+  if (userScore == computerScore) {
+    gameWinnerDisplay.textcontent = 'Game Draw'
+  } else if (userScore > computerScore) {
+    gameWinnerDisplay.textContent = 'U Won'
+  } else {
+    gameWinnerDisplay.textContent = 'Computer Won'
+  }
+  gameEnding()
+}
+
+function gameEnding() {
+  let resetBtn = document.createElement('button')
+  resetBtn.textContent = 'reset'
+  resetBtn.addEventListener('click', resetGame)
+  gameCards.appendChild(resetBtn)
+}
+
+
+// checking the player who won the round
+function playRound(user, computer) {
+  if (user === computer) {
+    console.log('draw')
   } else if (
-    (humanChoice == 'rock' && computerChoice == 'scissors') ||
-    (humanChoice == 'paper' && computerChoice == 'rock') ||
-    (humanChoice == 'scissors' && computerChoice == 'paper') 
+    (user == 'rock' && computer == 'scissors') ||
+    (user == 'paper' && computer == 'rock') ||
+    (user == 'scissors' && computer == 'paper')
   ) {
-    // make capitalize result
-    humanChoice = getCapitalizeResult(humanChoice)
-    computerChoice = getCapitalizeResult(computerChoice)
-    // showing the player | user winning messages
-    console.log(`You Wins! "${humanChoice}" beats "${computerChoice}"`)
-    // ading user score   
-    addingScores('user')
-    theWinner = 'human'
-  
+    console.log('user wins')
+    userScore++
+    userScoreDisplay.textContent = userScore
   } else {
-    humanChoice = getCapitalizeResult(humanChoice)
-    computerChoice = getCapitalizeResult(computerChoice)
-    // showing the computer winning messages
-    console.log(`You Loss! "${computerChoice}" beats "${humanChoice}"`)
-    // adding computer score
-    addingScores('computer')
-    
-  }
-
-}
-
-
-// ading scores both players
-function addingScores(winner) {
-  if (winner == 'user') {
-    humanScore += 1
-    userScoreDisplay.textContent = humanScore
-  } else {
-    computerScore += 1
+    console.log('computer wins')
+    computerScore++
     computerScoreDisplay.textContent = computerScore
   }
 }
 
-
-// the game will play rounds
-function playGame() {
-  const humanSelection = getHumanChoice()
-  const computerSelection = getComputerChoice()
-  playRound(humanSelection, computerSelection)
+function gettingComputerSelection() {
+  let computerChoice = ''
+  let randomNumber = Math.floor(Math.random() * 3)
+  switch (randomNumber) {
+    case 0:
+      computerChoice = 'rock'
+      break
+    case 1:
+      computerChoice = 'scissors'
+      break
+    case 2:
+      computerChoice = 'paper'
+      break
+  }
+  computerCard.textContent = computerChoice
+  return computerChoice
 }
 
-function checkingWinner() {
-  let winner = 'you' 
-  if (computerScore == humanScore) {
-    winner = 'Draw'
-
-  } else if (computerScore > humanScore) {
-    winner = 'computer'
-  }
-  return winner
+function gettingUserSelection(choice) {
+  userCard.textContent = choice
+  return choice
 }
 
-function startingGame() {
-  for (let i = 1; i <= 5; i++) {
-    playGame()
-  }
-  // checking game winner after end the 5 rounds
-  let winner = checkingWinner()
-  if (winner == 'Draw') {
-    winnerAlert.textContent = 'The Game will Draw'
 
-  } else if (winner == 'you') {
-    console.log('you are the main winner')
-    winnerAlert.textContent = getCapitalizeResult(winner)
-    winnerAlert.textContent += ' are the winner'
-
-  } else {
-    winnerAlert.textContent = getCapitalizeResult(winner)
-    winnerAlert.textContent += ' is the winner'
-  }
-}
-
-// game Starting 
-gameStartBtn.addEventListener('click', (e) => {
-  startingGame()
-})
